2c4b8a83de0c9a1ee8b576730b7c744325ec3631
thatFemicode <leyedavid10@gmail.com>
2021-09-17 21:33:54 +0100

    finished styling and fe

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import headline from '../headline/headline.css';
 import headlines from '../headline/headline';
 import sass from '../scss/main.scss';
+
+window.onload = function () {
+  console.log('me');
+};
